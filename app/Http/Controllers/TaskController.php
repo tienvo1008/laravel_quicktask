29d08b3dec0eec2069300de463ab08de4aeaf5db
Tien Vo <tienvo1008@gmail.com>
2018-12-27 16:49:56 +0700

    Building Layouts and views

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('tasks.index');
+    }
+    public function store(RequestTask $request)
+    {
+
+    }
+}
